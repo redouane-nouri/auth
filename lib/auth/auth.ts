9e7e0e2da7bfef7a14705900c31424db64e0bd04
Redouane Nouri <nour1r3d1@gmail.com>
2025-12-07 14:55:18 +0100

    feat(auth): prepare auth.js routing

---
+++ lib/auth/auth.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [ Credentials({
+    credentials: {username:{}, pasword:{}},
+    authorize: async (credentials) => {
+      return null;
+    },
+  }),],
+  pages:{
+    signIn: '/connect',
+  }
+})
