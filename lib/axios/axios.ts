3067f0d14b92d02cd65b075062e65ad293e0ff11
Redouane Nouri <nour1r3d1@gmail.com>
2025-12-08 16:44:12 +0100

    feat(axios): add a base url

--- lib/axios/axios.ts
+++ lib/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const Axios = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_URL,
+export const api = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_URL}/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
