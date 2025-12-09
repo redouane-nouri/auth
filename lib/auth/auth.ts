3bd90df18f3b61b84e4391da7079b07aaa63b688
Redouane Nouri <nour1r3d1@gmail.com>
2025-12-09 12:43:01 +0100

    feat(prisma): add authjs prisma adapter for db integration

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,15 +1,19 @@
 import { getUserSignInSchema } from "@/utils/functions";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { getTranslations } from "next-intl/server";
+import prisma from "../prisma/prisma-client";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   basePath: process.env.AUTH_BASEPATH,
+  adapter: PrismaAdapter(prisma),
   providers: [
     Credentials({
       credentials: { email: {}, password: {} },
       authorize: async (credentials) => {
         //TODO: Credential authz logic here
+        return null;
       },
     }),
   ],
