32bbac590ef8611e6b181320eeafcac11486ef2f
Redouane Nouri <nour1r3d1@gmail.com>
2025-12-09 15:13:49 +0100

    feat(auth): add custom error class for credentials authentication

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,9 +1,16 @@
 import { getUserSignInSchema } from "@/utils/functions";
-import NextAuth from "next-auth";
+import NextAuth, { CredentialsSignin } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { getTranslations } from "next-intl/server";
 import prisma from "../prisma/prisma-client";
+
+class CredentialsSigninError extends CredentialsSignin {
+  constructor(code: string) {
+    super();
+    this.code = code;
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   basePath: process.env.AUTH_BASEPATH,
@@ -12,8 +19,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Credentials({
       credentials: { email: {}, password: {} },
       authorize: async (credentials) => {
-        //TODO: Credential authz logic here
-        return null;
+        throw new CredentialsSigninError("TODO");
       },
     }),
   ],
