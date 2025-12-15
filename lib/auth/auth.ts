d17c5898d4884549c3b378899977f2e7a219b6db
Redouane Nouri <nour1r3d1@gmail.com>
2025-12-15 12:12:50 +0100

    feat(auth): customize magiclink email

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -9,6 +9,9 @@ import { v4 as uuidv4 } from "uuid";
 import prisma from "../prisma/prisma-client";
 import bcrypt from "bcrypt";
 import { createTransport } from "nodemailer";
+import { render } from "@react-email/render";
+import EmailHtml from "@/components/auth/EmailHtml";
+import React from "react";
 /*
   Customizable code message to show the user in credentials authentication
 */
@@ -27,56 +30,9 @@ type CredentialsT = {
   csrfToken: string;
   callbackUrl: string;
 };
-function html(params: { url: string; host: string; theme: any }) {
-  const { url, host, theme } = params;
-
-  const escapedHost = host.replace(/\./g, "&#8203;.");
-
-  const brandColor = theme.brandColor || "#346df1";
-  const color = {
-    background: "#f9f9f9",
-    text: "#444",
-    mainBackground: "#fff",
-    buttonBackground: brandColor,
-    buttonBorder: brandColor,
-    buttonText: theme.buttonText || "#fff",
-  };
-
-  return `
-<body style="background: ${color.background};">
-  <table width="100%" border="0" cellspacing="20" cellpadding="0"
-    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
-    <tr>
-      <td align="center"
-        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
-        Sign in to <strong>${escapedHost}</strong>
-      </td>
-    </tr>
-    <tr>
-      <td align="center" style="padding: 20px 0;">
-        <table border="0" cellspacing="0" cellpadding="0">
-          <tr>
-            <td align="center" style="border-radius: 5px;" bgcolor="${color.buttonBackground}"><a href="${url}"
-                target="_blank"
-                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${color.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${color.buttonBorder}; display: inline-block; font-weight: bold;">Sign
-                in</a></td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-    <tr>
-      <td align="center"
-        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
-        If you did not request this email you can safely ignore it.
-      </td>
-    </tr>
-  </table>
-</body>
-`;
-}
 
 // Email Text body (fallback for email clients that don't render HTML, e.g. feature phones)
-function text({ url, host }: { url: string; host: string }) {
+function text(url: string, host: string): string {
   return `Sign in to ${host}\n${url}\n\n`;
 }
 /*
@@ -158,13 +114,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         },
       },
       from: process.env.EMAIL_FROM,
-      async sendVerificationRequest({ identifier, url, provider, theme }) {
+      async sendVerificationRequest({ identifier, url, provider }) {
         const user = await prisma.user.findUnique({
           where: { email: identifier },
         });
 
         if (!user) {
-          console.log("***********************");
           return;
         }
 
@@ -173,9 +128,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         const result = await transport.sendMail({
           to: identifier,
           from: provider.from,
-          subject: `Sign in to ${host}`,
-          text: text({ url, host }),
-          html: html({ url, host, theme }),
+          subject: "Signin Link",
+          text: text(url, host),
+          html: await render(
+            React.createElement(EmailHtml, {
+              url,
+              host,
+            })
+          ),
         });
         const failed = result.rejected.concat(result.pending).filter(Boolean);
         if (failed.length) {
