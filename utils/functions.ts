b314695bdf976993f781318105162883b05c4d23
Redouane Nouri <nour1r3d1@gmail.com>
2026-01-26 15:20:18 +0100

    chore: add get reset password schema function

--- utils/functions.ts
+++ utils/functions.ts
@@ -98,3 +98,30 @@ export const getForgotPasswordSchema = (t: any) => {
     })
     .strict(t("validAttributes"));
 };
+/**
+ * Gets reset password zod validation schema
+ * @param t - next-int messages function, used for showing i18n errors.
+ * @returns zod schema
+ */
+export const getResetPasswordSchema = (t: any) => {
+  return z
+    .object({
+      token: z
+        .string({ message: t("tokenString") })
+        .min(1, t("tokenRequired")),
+      password: z
+        .string({ message: t("passwordString") })
+        .min(8, t("passwordMin"))
+        .max(60, t("passwordMax"))
+        .regex(/[a-z]/, t("passwordRegexLowercase"))
+        .regex(/[A-Z]/, t("passwordRegexUppercase"))
+        .regex(/[0-9]/, t("passwordRegexNumber"))
+        .regex(/[!-\/:-@[-`{-~]/, t("passwordSpecialCharacter")),
+      confirmPassword: z.string({ message: t("confirmPasswordString") }),
+    })
+    .strict(t("validAttributes"))
+    .refine((data) => data.password === data.confirmPassword, {
+      message: t("passwordsDontMatch"),
+      path: ["confirmPassword"],
+    });
+};
